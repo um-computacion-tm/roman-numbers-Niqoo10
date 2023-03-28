572f3282f7561bd953df02a74f0bb55f8c4c6f4d
Niqoo10 <nicopanella2009@hotmail.com>
2023-03-27 21:29:43 -0300

    ROMANOS

--- numeros.py
+++ numeros.py
@@ -1,24 +1,21 @@
 import unittest
-import math
 
-def decimal_to_roman(decimal):
+#PARTE DE CODIGO
+def decimal_to_roman(num):
+ 
+    c = [ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" ]
+    x = [ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" ]
+    i = [ "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" ]
 
-Unidad=["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-Decena=["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-Centena=["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-N=int(input("Ingresa numero entero\n"))
-u= N % 10
-d=int(math.floor(N/10))%10
-c=int(math.floor(N/100))
-if(N>=100): 
- print(Centena[c]+Decena[d]+Unidad[u])
-else:
- if(N>=10):
-  print(Decena[d]+Unidad[u])
- else:
-  print(Unidad[N])
+    centena = c[(num % 1000) // 100]
+    decena = x[(num % 100) // 10]
+    unidad = i[num % 10]
+
+    resultado = ( centena + decena + unidad)
+    return resultado
 
 
+#PARTE DE TEST
 class TestDecimalToRoman(unittest.TestCase):
     def test_uno(self):
         resultado = decimal_to_roman(1)
@@ -39,6 +36,34 @@ class TestDecimalToRoman(unittest.TestCase):
     def test_diez(self):
         resultado = decimal_to_roman(10)
         self.assertEqual(resultado, 'X')
+
+    def test_quince(self):
+        resultado = decimal_to_roman(15)
+        self.assertEqual(resultado, 'XV')
+
+    def test_veinte(self):
+        resultado = decimal_to_roman(20)
+        self.assertEqual(resultado, 'XX')
+
+    def test_treintaycuatro(self):
+        resultado = decimal_to_roman(34)
+        self.assertEqual(resultado, 'XXXIV')
+
+    def test_cincuenta(self):
+        resultado = decimal_to_roman(50)
+        self.assertEqual(resultado, 'L')
+
+    def test_noventaynueve(self):
+        resultado = decimal_to_roman(99)
+        self.assertEqual(resultado, 'XCIX')
+
+    def test_cien(self):
+        resultado = decimal_to_roman(100)
+        self.assertEqual(resultado, 'C')
+
+    def test_novecientonoventaynueve(self):
+        resultado = decimal_to_roman(999)
+        self.assertEqual(resultado, 'CMXCIX')
 
 
 if __name__ == '__main__':
